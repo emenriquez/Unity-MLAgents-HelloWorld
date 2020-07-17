d44624b74778ae4ff1e7817bb5fdb59655150a5b
Erik <uss593@my.utsa.edu>
2020-07-17 16:26:31 -0500

    initial commit

---
+++ Assets/RollerAgent.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class RollerAgent : Agent
+{
+    Rigidbody rB;
+
+    public Transform Target;
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rB = GetComponent<Rigidbody>();
+    }
+
+    public override void OnEpisodeBegin() {
+        if (this.transform.localPosition.y < 0) {
+            this.rB.angularVelocity = Vector3.zero;
+            this.rB.velocity = Vector3.zero;
+            this.transform.localPosition = new Vector3(0, 0.5f, 0);
+        }
+
+        // Move the target to a random new spot to start
+        Target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
+    }
+
+    public override void CollectObservations(VectorSensor sensor) {
+        // Target and Agent positions
+        sensor.AddObservation(Target.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
+
+        // Info on agent's velocity
+        sensor.AddObservation(rB.velocity.x);
+        sensor.AddObservation(rB.velocity.z);
+    }
+
+    public override void OnActionReceived(float[] vectorAction) {
+        // 2 possible actions
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = vectorAction[0];
+        controlSignal.z = vectorAction[1];
+        rB.AddForce(controlSignal * speed);
+
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+
+        // Reach the target
+        if (distanceToTarget < 1.42f) {
+            SetReward(1.0f);
+            EndEpisode();
+        }
+
+        // Fall off platform condition
+        if (this.transform.localPosition.y < 0) {
+            EndEpisode();
+        }
+    }
+
+    public override void Heuristic(float[] actionsOut) {
+        actionsOut[0] = Input.GetAxis("Horizontal");
+        actionsOut[1] = Input.GetAxis("Vertical");
+    }
+}
